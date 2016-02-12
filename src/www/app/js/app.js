12b3d93b2090f24fc84da33be9cde54b991714ac
JCloudYu <j.cloud.yu@gmail.com>
2016-02-12 22:18:06 +0800

    Enhance demo code's readability

--- src/www/app/js/app.js
+++ src/www/app/js/app.js
@@ -1,43 +1,50 @@
-requirejs.config({
-	baseUrl:"js/modules",
-	paths:{
-		lib: '../',
-		cordova: '../../../cordova'
-	}
-});
-
-require( ['cordova'], function(){
-	var app = {
-		// Application Constructor
-		initialize: function() {
-			this.bindEvents();
-		},
-		// Bind Event Listeners
-		//
-		// Bind any events that are required on startup. Common events are:
-		// 'load', 'deviceready', 'offline', and 'online'.
-		bindEvents: function() {
-			document.addEventListener('deviceready', this.onDeviceReady, false);
-		},
-		// deviceready Event Handler
-		//
-		// The scope of 'this' is the event. In order to call the 'receivedEvent'
-		// function, we must explicitly call 'app.receivedEvent(...);'
-		onDeviceReady: function() {
-			app.receivedEvent('deviceready');
-		},
-		// Update DOM on a Received Event
-		receivedEvent: function(id) {
-			var parentElement = document.getElementById(id);
-			var listeningElement = parentElement.querySelector('.listening');
-			var receivedElement = parentElement.querySelector('.received');
-
-			listeningElement.setAttribute('style', 'display:none;');
-			receivedElement.setAttribute('style', 'display:block;');
-
-			console.log('Received Event: ' + id);
+(function(){
+	requirejs.config({
+		baseUrl:"js/modules",
+		paths: {
+			lib: '../',
+			cordova: '../../../cordova'
 		}
-	};
+	});
 
-	app.initialize();
-});
+	var dependecies = [
+//		'cordova'
+	];
+
+	require( dependecies, function(){
+
+		var
+		windowObj = $( window ),
+		viewportSize = {
+			width:	windowObj.width(),
+			height:	windowObj.height()
+		};
+
+		$( 'body > .stage' ).each(function(){
+			var target = $( this );
+
+			if ( !target.is('[data-base-width]') )
+				return;
+
+			var
+			bWidth	= parseInt( target.attr( 'data-base-width' ) ),
+			scale	= viewportSize.width / bWidth,
+			bHeight = Math.ceil( viewportSize.height / scale );
+
+			target.css({
+				width: bWidth, height: bHeight,
+				'transform-origin': '0 0',
+				'transform': 'scale3d( ' + scale + ',' + scale + ',' +  '1 )',
+				'-webkit-transform-origin': '0 0',
+				'-webkit-transform': 'scale3d( ' + scale + ',' + scale + ',' +  '1 )'
+			});
+		});
+
+		$( document )
+		.one( window.cordova ? 'deviceready' : 'system-ready', function(){
+			var parentElement	 = $( '[data-id="main-view"] .blink' );
+			parentElement.find( '.event' ).addClass( 'received' ).removeClass( 'listening' ).text( 'Device is Ready' );
+		})
+		.ready(function(){ $(document).trigger( 'system-ready' ); });
+	});
+})();
