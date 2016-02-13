05c99625102e42ab8345176b75799ae270dcdca4
JCloudYu <j.cloud.yu@gmail.com>
2016-02-13 21:50:11 +0800

    Rewrite app.js to load required js files before loading app dependent modules or controllers

--- www/app/js/app.js
+++ www/app/js/app.js
@@ -11,17 +11,23 @@
 		}
 	});
 
-	var dependecies = [
-//		'cordova'
+	var
+	prerequisites	 = [ 'constant' ],
+	app_dependencies = [
+		'cordova',
 		'module/scale-fix',
 
 		'controller/demo'
 	];
 
-	require( dependecies, function() {
-		$( document ).one( window.cordova ? 'deviceready' : 'system-ready', function(){
-			oops.runtime.fire( CORE.EVENT.SYNC_HEART_BEAT, 0 );
-		})
-		.ready(function(){ $(document).trigger( 'system-ready' ); });
+
+	require( prerequisites, function(){
+		require( app_dependencies, function() {
+			$( document )
+			.one( window.cordova ? 'deviceready' : 'system-ready', function(){
+				oops.runtime.fire( CORE.EVENT.SYNC_HEART_BEAT, 0 );
+			})
+			.ready(function(){ $(document).trigger( 'system-ready' ); });
+		});
 	});
 })();
