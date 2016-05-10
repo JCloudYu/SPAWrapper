f312ff0dea0dd1341f226f46ca387fd6be5b27d2
JCloudYu <j.cloud.yu@gmail.com>
2016-05-10 10:02:46 +0800

    Resize .stage's scale according to its parent's size

--- www/app/js/module/scale-fix.js
+++ www/app/js/module/scale-fix.js
@@ -1,25 +1,31 @@
 (function() {
 	"use strict";
 
-	var
-	windowObj = $( window ),
-	viewportSize = {
-		width:	windowObj.width(),
-		height:	windowObj.height()
-	};
 
-	$( 'body > .stage' ).each(function(){
-		var target = $( this );
 
-		if ( !target.is('[data-base-width]') )
-			return;
+	$( '.stage' ).each(function(){
+		var
+		target = $( this ),
+		parent = target.parent();
+
+
+		if ( !target.is('[data-base-width]') ) return;
+
+
 
 		var
+		parentSize = {
+			width:  parent.width(),
+			height: parent.height()
+		},
 		bWidth	= parseInt( target.attr( 'data-base-width' ) ),
-		scale	= viewportSize.width / bWidth,
-		bHeight = Math.ceil( viewportSize.height / scale );
+		scale	= parentSize.width / bWidth,
+		bHeight = Math.ceil( parentSize.height / scale );
 
-		target.css({
+
+		target
+		.attr( 'data-scale', scale )
+		.css({
 			width: bWidth, height: bHeight,
 			'transform-origin': '0 0',
 			'transform': 'scale3d( ' + scale + ',' + scale + ',' +  '1 )',
