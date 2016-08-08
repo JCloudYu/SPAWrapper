2971ecf88b16a9e985fdd45f57cc14f99a249491
JCloudYu <j.cloud.yu@gmail.com>
2016-08-08 12:52:11 +0800

    Isolate main-view components from boot app structure

--- www/comps/MainView/comp.js
+++ www/comps/MainView/comp.js
@@ -1,8 +1,10 @@
 (function() {
 	"use strict";
 
-	oops.runtime.instantiate( function( kernelOp ){
-		kernelOp.on( CORE.EVENT.SYNC_HEART_BEAT, function(){
+	pump.instantiate( function( kernelOp ){
+		kernelOp.on( CORE.EVENT.SYNC_HEART_BEAT, function( e, stage ){
+			if ( stage != "boot final" ) return;
+		
 			var parentElement = $( '[data-id="main-view"] .blink' );
 			parentElement.find( '.event' ).addClass( 'received' ).removeClass( 'listening' ).text( 'Device is Ready' );
 		});
