b71f62182b41c6129b94fda5eed50d22ef2d6c84
JCloudYu <j.cloud.yu@gmail.com>
2016-09-14 08:33:00 +0800

    Add preferences.parser api to support custom type casting of input app preference

--- www/js/module/pref.js
+++ www/js/module/pref.js
@@ -5,19 +5,29 @@
 	
 	
 	
-	var __preferences = {};
-	$U.merge( window.env, {
-		preference:function Preference( prefName ){
-			return __preferences[ prefName.toLowerCase() ];
-		}
-	}, true, true );
+	var
+	__preferences = {},
+	__parser = ___BYPASS,
+	__prefReader = function( prefName ) {
+		return __parser( prefName, __preferences[ prefName.toLowerCase() ] );
+	};
+	
+	
+	
+	__prefReader.parser = function( parser ){
+		if ( !parser || __preferences.toString.call(parser) !== '[object Function]' ) return;
+		__parser = parser;
+	};
+	
+	$U.merge( window.env, { preference:__prefReader }, true, true );
 	
 	
 	
 	module.signal = new Promise(function( fulfill ){
 		cordova.preferences.all(function( pref ){
-			__preferences = pref;
-			fulfill();
+			__preferences = pref; fulfill();
 		});
 	});
+	
+	function ___BYPASS( name, value ){ return value; }
 })();
