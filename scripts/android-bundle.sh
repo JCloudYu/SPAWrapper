af4ba6df32f2c38e5409668356f51a49372b90fa
JCloudYu <j.cloud.yu@gmail.com>
2016-11-07 11:09:40 +0800

    Isolate external resources from building environment

--- scripts/android-bundle.sh
+++ scripts/android-bundle.sh
@@ -9,7 +9,7 @@ echo "Copying bundle resources...";
 if [ -d "../res/raw" ]; then
 	mkdir -p ../platforms/android/res/raw
 	rm -rf ../platforms/android/res/raw/*
-	cp -rf ../res/raw/* ../platforms/android/res/raw/
+	cp -rf ../external/raw/* ../platforms/android/res/raw/
 else
 	echo "Nothing to copy!";
 fi;
